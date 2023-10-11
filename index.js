e576f9c1da51eb5304b8d8b1121292812e2167d4
Real-Chaos <rizwannafees13@gmail.com>
2023-10-10 21:01:22 -0400

    Finished building the game

---
+++ index.js
@@ -0,0 +1,70 @@
+// Psuedo code
+
+/* 
+
+
+
+1) Create function called getComputerChoice 
+  a) returns rock paper or scissors randomly
+2) create playRound() function 
+  a) takes two parameters: playerSelection and computerSelection
+  b) returns a string depending on who wins
+3) create game() function
+  a) plays 5 rounds
+  b) keeps track of scores
+  c) at the end of round 5, displays who won
+
+  use prompt() to get players choice 
+
+
+*/
+
+const getComputerChoice = () => {
+  const chooseRandom = Math.floor(Math.random() * 3) + 1
+  switch (chooseRandom) {
+    case 1:
+      return 'rock'
+      break
+    case 2:
+      return 'paper'
+      break
+    case 3:
+      return 'scissors'
+      break
+  }
+}
+
+const playRound = (computerChoice, playerSelection) => {
+  if (computerChoice === playerSelection) return 'tied!'
+  else if (computerChoice === 'rock' && playerSelection === 'scissors') {
+    return 'You Lost'
+  } else if (computerChoice === 'paper' && playerSelection === 'rock') {
+    return 'You Lost'
+  }
+  if (computerChoice === 'scissors' && playerSelection === 'paper') {
+    return 'You Lost'
+  } else return 'You Won'
+}
+
+const game = () => {
+  let playerScore = 0
+  let computerScore = 0
+  for (let i = 0; i < 5; i++) {
+    const computerChoice = getComputerChoice()
+    const playerSelection = prompt('Choose rock, paper, or scissors')
+    const roundResults = playRound(computerChoice, playerSelection)
+    roundResults === 'You Won'
+      ? playerScore++
+      : roundResults === 'You Lost'
+      ? computerScore++
+      : null
+  }
+
+  const returnMessage = `The Computer won ${computerScore} rounds and you won ${playerScore} rounds.`
+
+  if(playerScore > computerScore) return `${returnMessage} You win!`
+  else if(playerScore > computerScore) return `${returnMessage} You lost!`
+  else return `${returnMessage}. The game is Tied!`
+}
+
+console.log(game())
